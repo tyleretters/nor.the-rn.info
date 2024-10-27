8e82258e7d1daca777758106e15868d5a2e0556c
Tyler Etters <tyler@etters.co>
2024-10-26 19:38:47 -0700

    install discography

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import discography from '@tyleretters/discography'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+
+console.log(discography)
 
 function App() {
   const [count, setCount] = useState(0)
