ed6113f74f1fb6c7db66ca42d42d324430b29633
Tyler Etters <tyler@etters.co>
2024-10-26 19:50:23 -0700

    install landscape & svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,16 @@
 import discography from '@tyleretters/discography'
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import png000558300025 from './assets/000558300025.png'
+import northernInformationAppliedSciencesAndPhantasmsWorkingDivision from '/northern-information-applied-sciences-and-phantasms-working-division.svg'
 import './App.css'
 
 console.log(discography)
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="">
+      <img src={northernInformationAppliedSciencesAndPhantasmsWorkingDivision} alt="Northern Information Applied Sciences and Phantasms Working Division" />
+      <img src={png000558300025} alt="000558300025" />
+    </div>
   )
 }
 
