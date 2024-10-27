3412fb530003c91ecf6659b506d2a39bf480eb2f
Tyler Etters <tyler@etters.co>
2024-10-26 20:34:03 -0700

    install all the css

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import discography from '@tyleretters/discography'
 import png000558300025 from './assets/000558300025.png'
 import northernInformationAppliedSciencesAndPhantasmsWorkingDivision from '/northern-information-applied-sciences-and-phantasms-working-division.svg'
-import './App.css'
 
 console.log(discography)
 
 function App() {
   return (
     <div className="">
-      <img src={northernInformationAppliedSciencesAndPhantasmsWorkingDivision} alt="Northern Information Applied Sciences and Phantasms Working Division" />
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <img
+        src={northernInformationAppliedSciencesAndPhantasmsWorkingDivision}
+        alt="Northern Information Applied Sciences and Phantasms Working Division"
+      />
       <img src={png000558300025} alt="000558300025" />
     </div>
   )
