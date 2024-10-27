a80c3ce18c16a02c0ee680e499a15325c58ee7ff
Tyler Etters <tyler@etters.co>
2024-10-26 21:03:33 -0700

    install light/dark mode toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
 import discography from '@tyleretters/discography'
 import png000558300025 from './assets/000558300025.png'
 import northernInformationAppliedSciencesAndPhantasmsWorkingDivision from '/northern-information-applied-sciences-and-phantasms-working-division.svg'
+import { ThemeProvider } from '@/components/theme-provider'
+import { ModeToggle } from '@/components/mode-toggle'
 
 console.log(discography)
 
 function App() {
   return (
-    <div className="">
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <ModeToggle />
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <img
         src={northernInformationAppliedSciencesAndPhantasmsWorkingDivision}
         alt="Northern Information Applied Sciences and Phantasms Working Division"
       />
       <img src={png000558300025} alt="000558300025" />
-    </div>
+    </ThemeProvider>
   )
 }
 
