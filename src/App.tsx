9149e8302b428a238e52598c571d74349a6efb49
Tyler Etters <tyler@etters.co>
2024-11-02 11:15:08 -0700

    clean up App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter } from 'react-router-dom'
 import discography from '@tyleretters/discography'
-import png000558300025 from './assets/000558300025.png'
-import northernInformationAppliedSciencesAndPhantasmsWorkingDivision from '/northern-information-applied-sciences-and-phantasms-working-division.svg'
+// import png000558300025 from './assets/000558300025.png'
+// import northernInformationAppliedSciencesAndPhantasmsWorkingDivision from '/northern-information-applied-sciences-and-phantasms-working-division.svg'
 import { ThemeProvider } from '@/components/theme-provider'
 import { ModeToggle } from '@/components/mode-toggle'
 
@@ -11,13 +11,16 @@ function App() {
   return (
     <BrowserRouter basename="/rm_ation">
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <ModeToggle />
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        <img
-          src={northernInformationAppliedSciencesAndPhantasmsWorkingDivision}
-          alt="Northern Information Applied Sciences and Phantasms Working Division"
-        />
-        <img src={png000558300025} alt="000558300025" />
+        <div className="m-10 flex justify-between">
+          {/* <img
+              src={northernInformationAppliedSciencesAndPhantasmsWorkingDivision}
+              alt="Northern Information Applied Sciences and Phantasms Working Division"
+              className="w-[64px]"
+              /> */}
+          <h1 className="text-3xl font-bold">nor.the-rn.info/rm_ation</h1>
+          <ModeToggle />
+          {/* <img src={png000558300025} alt="000558300025" /> */}
+        </div>
       </ThemeProvider>
     </BrowserRouter>
   )
