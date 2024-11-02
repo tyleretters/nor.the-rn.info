4ca43017d665a1b51f9bed9947a03065c108245d
Tyler Etters <tyler@etters.co>
2024-11-02 13:55:26 -0700

    add powerlines

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from 'react-router-dom'
 import discography from '@tyleretters/discography'
-// import png000558300025 from './assets/000558300025.png'
+import png000558300025 from './assets/000558300025.png'
 // import northernInformationAppliedSciencesAndPhantasmsWorkingDivision from '/northern-information-applied-sciences-and-phantasms-working-division.svg'
 import { ThemeProvider } from '@/components/theme-provider'
 import { ModeToggle } from '@/components/mode-toggle'
@@ -11,15 +11,17 @@ function App() {
   return (
     <BrowserRouter basename="/rm_ation">
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <div className="m-10 flex justify-between">
-          {/* <img
-              src={northernInformationAppliedSciencesAndPhantasmsWorkingDivision}
-              alt="Northern Information Applied Sciences and Phantasms Working Division"
-              className="w-[64px]"
-              /> */}
-          <h1 className="text-3xl">nor.the-rn.info/rm_ation</h1>
-          <ModeToggle />
-          {/* <img src={png000558300025} alt="000558300025" /> */}
+        <div className="container mx-auto">
+          <div className="my-5 mx-5 md:mx-0 flex justify-between">
+            {/* <img
+                src={northernInformationAppliedSciencesAndPhantasmsWorkingDivision}
+                alt="Northern Information Applied Sciences and Phantasms Working Division"
+                className="w-[64px]"
+                /> */}
+            <h1 className="text-3xl">nor.the-rn.info/rm_ation</h1>
+            <ModeToggle />
+          </div>
+          <img src={png000558300025} className="object-fill" alt="000558300025" />
         </div>
       </ThemeProvider>
     </BrowserRouter>
