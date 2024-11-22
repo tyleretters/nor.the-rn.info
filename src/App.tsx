978115798a05b87cdca6f193a42221694e224085
Tyler Etters <tyler@etters.co>
2024-11-21 19:00:56 -0800

    refactor for fast refresh

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { BrowserRouter } from 'react-router-dom'
 import discography from '@tyleretters/discography'
 // import png000558300025 from './assets/000558300025.png'
 import northernInformationAppliedSciencesAndPhantasmsWorkingDivision from '/northern-information-applied-sciences-and-phantasms-working-division.svg'
-import { ThemeProvider } from '@/components/theme-provider'
-import { ModeToggle } from '@/components/mode-toggle'
+import { ThemeProvider } from '@/components/themeProvider'
+import { ModeToggle } from '@/components/modeToggle'
 import Release from '@/components/release'
 
 console.log(discography)
@@ -13,23 +13,20 @@ function App() {
     <BrowserRouter basename="/rm_ation">
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <div className="container mx-auto">
-          <div className="my-5 mx-5 md:mx-0 flex justify-between">
+          <div className="mx-5 my-5 flex justify-between md:mx-0">
             <img
-                src={northernInformationAppliedSciencesAndPhantasmsWorkingDivision}
-                alt="Northern Information Applied Sciences and Phantasms Working Division"
-                className="w-[64px]"
-                />
+              src={
+                northernInformationAppliedSciencesAndPhantasmsWorkingDivision
+              }
+              alt="Northern Information Applied Sciences and Phantasms Working Division"
+              className="w-[64px]"
+            />
             <h1 className="text-3xl">nor.the-rn.info/rm_ation</h1>
             <ModeToggle />
           </div>
           <ul className="releases">
             {discography.map((release) => {
-              return (
-                <Release
-                  key={release.id}
-                  release={release}
-                />
-              )
+              return <Release key={release.id} release={release} />
             })}
           </ul>
           {/* <img src={png000558300025} className="object-fill" alt="000558300025" /> */}
