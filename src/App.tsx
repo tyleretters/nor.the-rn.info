66a176c167e290e3fd0eb10b2664f4b65fe2f37b
Tyler Etters <tyler@etters.co>
2024-11-28 13:42:58 -0800

    simplify

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
 import { BrowserRouter } from 'react-router-dom'
-import discography from '@tyleretters/discography'
-// import png000558300025 from './assets/000558300025.png'
-import northernInformationAppliedSciencesAndPhantasmsWorkingDivision from '/northern-information-applied-sciences-and-phantasms-working-division.svg'
+import logo from '/northern-information-applied-sciences-and-phantasms-working-division.svg'
 import { ThemeProvider } from '@/components/themeProvider'
 import { ModeToggle } from '@/components/modeToggle'
-import Release from '@/components/release'
-
-console.log(discography)
+import { Button } from '@/components/ui/button'
 
 function App() {
   return (
     <BrowserRouter basename="/rm_ation">
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <div className="container mx-auto">
-          <div className="mx-5 my-5 flex justify-between md:mx-0">
-            <img
-              src={
-                northernInformationAppliedSciencesAndPhantasmsWorkingDivision
-              }
-              alt="Northern Information Applied Sciences and Phantasms Working Division"
-              className="w-[64px]"
-            />
-            <h1 className="text-3xl">nor.the-rn.info/rm_ation</h1>
+          <div className="mx-5 my-5 flex justify-between">
+            <Button variant="outline" asChild>
+              <a className="" href="https://nor.the-rn.info">
+                &larr; Back
+              </a>
+            </Button>
             <ModeToggle />
           </div>
-          <ul className="releases">
-            {discography.map((release) => {
-              return <Release key={release.id} release={release} />
-            })}
-          </ul>
-          {/* <img src={png000558300025} className="object-fill" alt="000558300025" /> */}
+        </div>
+        <div className="container mx-auto flex place-content-center">
+          <img
+            src={logo}
+            alt="Northern Information Applied Sciences and Phantasms Working Division"
+            className="h-auto w-auto p-5 md:w-auto md:max-w-[700px]"
+          />
         </div>
       </ThemeProvider>
     </BrowserRouter>
