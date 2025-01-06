fab819d913e701add1101b9dc89590e6ba672cf6
Tyler Etters <tyler@etters.co>
2025-01-05 18:21:26 -0800

    tidy

--- src/data/site.js
+++ src/data/site.js
@@ -1,24 +1,22 @@
 import { META } from '../../eleventy.config.js'
 
-export const ACCOUNTS = {
-  BANDCAMP: 'https://intertext.bandcamp.com',
-  BLUESKY: 'https://bsky.app/profile/tyleretters.bsky.social',
-  DISCORD: 'https://discord.gg/nYycWdsT5n',
-  EMAIL: 'tyler@etters.co',
-  GEMINI: 'gemini://tilde.club/~tse/',
-  GITHUB: 'https://github.com/tyleretters',
-  INSTAGRAM: 'https://instagram.com/tyleretters',
-  LINKEDIN: 'https://www.linkedin.com/in/tyleretters',
-  MASTODON: 'https://merveilles.town/web/@tyleretters',
-  NEWSLETTER:
-    'https://us1.campaign-archive.com/home/?u=e82006751a8517b8fd440a182&68446b3abb',
-  SOUNDCLOUD: 'https://soundcloud.com/tyleretters',
-  TWITCH: 'https://twitch.tv/tyleretters',
-  X: 'https://x.com/tyleretters',
-  YOUTUBE: 'https://www.youtube.com/channel/UCkZn9WNgPM3gVf9fiyuN1Mg',
-}
-
 export default {
   META,
-  ACCOUNTS,
+  ACCOUNTS: {
+    BANDCAMP: 'https://intertext.bandcamp.com',
+    BLUESKY: 'https://bsky.app/profile/tyleretters.bsky.social',
+    DISCORD: 'https://discord.gg/nYycWdsT5n',
+    EMAIL: 'tyler@etters.co',
+    GEMINI: 'gemini://tilde.club/~tse/',
+    GITHUB: 'https://github.com/tyleretters',
+    INSTAGRAM: 'https://instagram.com/tyleretters',
+    LINKEDIN: 'https://www.linkedin.com/in/tyleretters',
+    MASTODON: 'https://merveilles.town/web/@tyleretters',
+    NEWSLETTER:
+      'https://us1.campaign-archive.com/home/?u=e82006751a8517b8fd440a182&68446b3abb',
+    SOUNDCLOUD: 'https://soundcloud.com/tyleretters',
+    TWITCH: 'https://twitch.tv/tyleretters',
+    X: 'https://x.com/tyleretters',
+    YOUTUBE: 'https://www.youtube.com/channel/UCkZn9WNgPM3gVf9fiyuN1Mg',
+  },
 }
