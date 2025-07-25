80ffb0301e174263a916978713e58106deba4369
Tyler Etters <tyler.etters@discogsinc.com>
2025-07-25 14:08:35 -0700

    ++

--- src/data/navigation.js
+++ src/data/navigation.js
@@ -3,48 +3,40 @@ export default [
     url: '/music/',
     label: 'Music Releases',
     match: 'contains',
-    class: 'border-r-4 ',
   },
   {
     url: '/video/',
     label: 'Music Videos',
     match: 'contains',
-    class: 'md:border-r-4',
   },
   {
     url: '/photography/',
     label: 'Photography',
     match: 'equals',
-    class: 'border-r-4 ',
   },
   {
     url: '/about/',
     label: 'About',
     match: 'equals',
-    class: '',
   },
   {
     url: '/blog/',
     label: 'Blog',
     match: 'equals',
-    class: 'border-r-4 ',
   },
   {
     url: '/links/',
     label: 'Links',
     match: 'equals',
-    class: 'md:border-r-4',
   },
   {
     url: '/garden/',
     label: 'More',
     match: 'equals',
-    class: 'border-r-4',
   },
   {
     url: '/search/',
     label: 'Search',
     match: 'equals',
-    class: '',
   },
 ]
