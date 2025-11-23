0380f4932bd0b69c3de110ed94b4a0363e5304c5
Tyler Etters <tyler@etters.co>
2025-11-23 09:16:27 -0800

    ++

--- src/data/subsidiaries.js
+++ src/data/subsidiaries.js
@@ -8,7 +8,7 @@ export default [
   {
     title: 'Intertext',
     dates: '2018 - Present',
-    image: '/images/intertext-2024.png',
+    image: '/images/intertext-2025.png',
     content: `[Intertext](https://intertext.bandcamp.com) is where I release my music. It is a hermitic, one-man, non-label. Intertext has no branding, no lettermark, no mission, no social media presence, no marketing strategy. This paragraph is the only "about" section that exists. Mark Fisher introduced me to the word and if you're looking to get lost in a wormhole, I highly recommend [this Wikipedia article](https://en.wikipedia.org/wiki/Intertextuality).`,
   },
   {
