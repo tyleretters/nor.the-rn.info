abc1c3a9a4ea32ff1c9afc6ea3ff4cc6010ce6df
Tyler Etters <tyler.etters@discogsinc.com>
2026-02-17 08:19:37 -0800

    refactor photogrpahy

---
+++ src/data/photography.js
@@ -0,0 +1,106 @@
+export default [
+  {
+    url: '/images/000558300028.jpg',
+    title: '000558300028',
+  },
+  {
+    url: '/images/000537940012.jpg',
+    title: '000537940012',
+  },
+  {
+    url: '/images/09530002.jpg',
+    title: '09530002',
+  },
+  {
+    url: '/images/09560005.jpg',
+    title: '09560005',
+  },
+  {
+    url: '/images/09560015.jpg',
+    title: '09560015',
+  },
+  {
+    url: '/images/17540002.jpg',
+    title: '17540002',
+  },
+  {
+    url: '/images/98690022.jpg',
+    title: '98690022',
+  },
+  {
+    url: '/images/98690026.jpg',
+    title: '98690026',
+  },
+  {
+    url: '/images/98690029.jpg',
+    title: '98690029',
+  },
+  {
+    url: '/images/FH000024.jpg',
+    title: 'FH000024',
+  },
+  {
+    url: '/images/R1-05887-0005.jpg',
+    title: 'R1-05887-0005',
+  },
+  {
+    url: '/images/R1-07186-0032.jpg',
+    title: 'R1-07186-0032',
+  },
+  {
+    url: '/images/R1-07348-0010.jpg',
+    title: 'R1-07348-0010',
+  },
+  {
+    url: '/images/R1-07348-0017.jpg',
+    title: 'R1-07348-0017',
+  },
+  {
+    url: '/images/R1-07457-0006.jpg',
+    title: 'R1-07457-0006',
+  },
+  {
+    url: '/images/R1-07458-0019.jpg',
+    title: 'R1-07458-0019',
+  },
+  {
+    url: '/images/R1-2.jpg',
+    title: 'R1-2',
+  },
+  {
+    url: '/images/R1-21.jpg',
+    title: 'R1-21',
+  },
+  {
+    url: '/images/R1-26.jpg',
+    title: 'R1-26',
+  },
+  {
+    url: '/images/R1-28.jpg',
+    title: 'R1-28',
+  },
+  {
+    url: '/images/R1-4.jpg',
+    title: 'R1-4',
+  },
+  {
+    url: '/images/R1-6.jpg',
+    title: 'R1-6',
+  },
+  {
+    url: '/images/R1-9-1.jpg',
+    title: 'R1-9-1',
+  },
+  {
+    url: '/images/R1-9.jpg',
+    title: 'R1-9',
+  },
+  {
+    url: '/images/000558300027.jpg',
+    title: '000558300027',
+  },
+  {
+    url: '/images/000309410014.jpg',
+    title: '000309410014',
+  },
+]
